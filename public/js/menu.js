6dcbc80633d7899c03954c4006d88052a6fe9005
Rafael Fagundes <65747985+0xrsfagundes@users.noreply.github.com>
2022-12-22 21:19:29 +0000

    Thursday, December 22, 2022

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#header').append(`
+    $('#menu').append(`
     <nav class="navbar navbar-default">
     <div class="container-fluid">
       <div class="navbar-header">
