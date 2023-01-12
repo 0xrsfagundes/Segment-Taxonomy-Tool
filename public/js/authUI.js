062351be2e4a009336014aace5befd262f399e6a
Rafael Fagundes <65747985+0xrsfagundes@users.noreply.github.com>
2023-01-12 01:05:56 +0000

    now

--- public/js/authUI.js
+++ public/js/authUI.js
@@ -12,7 +12,7 @@ const uiConfig = {
   signInFlow: 'popup',
   signInSuccessUrl: './modules/index.html',
   signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
   ],
 };
